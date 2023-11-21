5b1d441c4620130ac1cef830404b8c03d873a168
qil008 <qil008@ucsd.edu>
2023-11-20 23:26:45 -0800

    整合支付宝

---
+++ src/main/java/com/julian/springbacken/controller/GeneralController.java
@@ -0,0 +1,14 @@
+package com.julian.springbacken.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GeneralController {
+
+    @GetMapping("/")
+    public String testReturn(){
+        return "success";
+    }
+
+}
