6b97b2487e581ccaa73bbf10014e5e0ed2c81184
qil008 <qil008@ucsd.edu>
2023-11-28 00:06:44 -0800

    mqtt回调类新增如果检测为行程topic则将message解剖并存入tracking库

--- src/main/java/com/julian/springbacken/mqtt/PushCallback.java
+++ src/main/java/com/julian/springbacken/mqtt/PushCallback.java
@@ -1,5 +1,6 @@
 package com.julian.springbacken.mqtt;
 
+import com.julian.springbacken.service.TrackService;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
@@ -13,6 +14,8 @@ public class PushCallback implements MqttCallback {
     @Autowired
     private MqttConfiguration mqttConfiguration;
 
+    @Autowired
+    private  TrackService trackService;
     @Override
     public void connectionLost(Throwable cause) {        // 连接丢失后，一般在这里面进行重连
         log.info("连接断开，正在重连");
@@ -34,7 +37,26 @@ public class PushCallback implements MqttCallback {
         // subscribe后得到的消息会执行到这里面,这里在控制台有输出
         log.info("接收消息主题 : " + topic);
         log.info("接收消息Qos : " + message.getQos());
-        log.info("接收消息内容 : " + new String(message.getPayload()));
+        String payload = new String(message.getPayload());
+        log.info("接收消息内容 : " + payload);
+        if (!topic.contains("-")) {
+            // 解析消息内容
+            String[] parts = payload.split("/");
+            if (parts.length == 5) {
+                try {
+                    Long rid = Long.parseLong(parts[0]);
+                    Double lng = Double.parseDouble(parts[1]);
+                    Double lat = Double.parseDouble(parts[2]);
+                    Double speed = Double.parseDouble(parts[3]);
+                    Double asl = Double.parseDouble(parts[4]);
+                    trackService.putTrack(rid, lng, lat, speed, asl);
+                } catch (NumberFormatException e) {
+                    log.error("解析错误: " + e.getMessage());
+                }
+            } else {
+                log.error("消息格式不正确");
+            }
+        }
 
     }
 
